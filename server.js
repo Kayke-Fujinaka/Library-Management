8d176a4c39dee60d472a194809e094ac9eef3dd2
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-01 17:07:42 -0300

    start project

---
+++ server.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import Fastify from "fastify";
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", (request, response) => {
+  response.send({ hello: "world" });
+});
+
+fastify.listen({ port, host }, (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
