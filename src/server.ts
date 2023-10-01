3a2ecc45b280c6b03403c2c16526d570d3b16ce0
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-01 17:36:16 -0300

    feat: handle server errors

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,9 @@ import 'dotenv/config';
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT, host: env.HOST });
+app.listen({ port: env.PORT, host: env.HOST }, (err) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
