f59cce6c81364ebeccee852d4b57f6bb5011507c
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-01 17:34:52 -0300

    config: setup zod

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import Fastify from 'fastify';
+import { ZodError } from 'zod';
+
+import { env } from './env';
+
+export const app = Fastify({
+  logger: true,
+});
+
+app.get('/', (request, response) => {
+  response.send({ hello: 'world' });
+});
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // TODO: Here we should log to on external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,6 @@
 import 'dotenv/config';
-import Fastify from 'fastify';
 
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || '0.0.0.0';
+import { app } from './app';
+import { env } from './env';
 
-const fastify = Fastify({
-  logger: true,
-});
-
-fastify.get('/', (request, response) => {
-  response.send({ hello: 'world' });
-});
-
-fastify.listen({ port, host }, (err) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-});
+app.listen({ port: env.PORT, host: env.HOST });
