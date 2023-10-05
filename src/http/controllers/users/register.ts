ec7f8a5dfea812960e9d50d7da33fe53fd0af0d8
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-04 21:37:03 -0300

    feat: create register route

---
+++ src/http/controllers/users/register.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { RegisterUseCase } from '@/use-cases/register';
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const useCase = new RegisterUseCase(usersRepository);
+
+    await useCase.execute({ name, email, password });
+  } catch (error: any) {
+    return reply.status(409).send({ message: error.message });
+  }
+}
