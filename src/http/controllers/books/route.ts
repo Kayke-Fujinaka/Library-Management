704f542af938d5b1be33cb7f67b1945633bbed93
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-15 12:36:25 -0300

    feat: add route to list books

--- src/http/controllers/books/route.ts
+++ src/http/controllers/books/route.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import { registerBook } from './register';
+import { listBooks } from './list-books';
 
 export async function booksRoutes(app: FastifyInstance) {
   app.post('/', registerBook);
+  app.get('/', listBooks);
 }
