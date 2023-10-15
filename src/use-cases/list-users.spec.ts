a4940bf9076ef254beb46fed4a5934b1cb4ce5f8
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-15 18:45:01 -0300

    test: list user use case

---
+++ src/use-cases/list-users.spec.ts
@@ -0,0 +1,42 @@
+import { UsersRepository } from '@/repositories/users-repository';
+import { ListUsersUseCase, SanitizedUser } from './list-users';
+
+describe('List Users Use Case', () => {
+  let sut: ListUsersUseCase;
+  let usersRepository: jest.Mocked<UsersRepository>;
+  let findManySpy: jest.SpyInstance;
+
+  beforeAll(async () => {
+    usersRepository = {
+      findMany: jest.fn(),
+    } as unknown as jest.Mocked<UsersRepository>;
+
+    findManySpy = jest.spyOn(usersRepository, 'findMany');
+
+    sut = new ListUsersUseCase(usersRepository);
+  });
+
+  const user: SanitizedUser = {
+    id: '9029a95c-c611-4a4a-8f6f-52350f7ab6ac',
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    created_at: new Date(),
+    updated_at: new Date(),
+    deleted_at: false,
+  };
+
+  it('should be defined', () => {
+    expect(usersRepository).toBeDefined();
+    expect(findManySpy).toBeDefined();
+  });
+
+  it('should be able to return all users', async () => {
+    const users = [user];
+
+    findManySpy.mockResolvedValue(users);
+
+    const result = await sut.execute();
+
+    expect(result.users).toEqual(users);
+  });
+});
