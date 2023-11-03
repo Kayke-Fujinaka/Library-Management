d94efe91d17c4d8d8a614a12ba4c2e13eaa0e184
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-03 15:42:21 -0300

    feat: authenticate route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from 'fastify';
 
+import { authenticate } from './authenticate';
 import { deleteUser } from './delete';
 import { listUsers } from './list';
 import { registerUser } from './register';
 
 export async function usersRoutes(app: FastifyInstance) {
+  app.post('/sessions', authenticate);
   app.post('/', registerUser);
   app.get('/', listUsers);
   app.delete('/', deleteUser);
