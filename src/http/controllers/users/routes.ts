fab55111fed84a00dd94ae7b17af6ca643837bfd
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-05 10:32:32 -0300

    feat: add refrresh token to routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,15 @@ import { FastifyInstance } from 'fastify';
 import { authenticate } from './authenticate';
 import { deleteUser } from './delete';
 import { listUsers } from './list';
+import { refresh } from './refresh';
 import { registerUser } from './register';
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post('/sessions', authenticate);
   app.post('/', registerUser);
+  app.post('/session', authenticate);
+
+  app.patch('/token/refresh', refresh);
+
   app.get('/', listUsers);
   app.delete('/', deleteUser);
 }
